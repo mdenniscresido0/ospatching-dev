6dd8e02f16229b68e0d107af92ec8533a023a5ce
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-04-24 07:39:11 +0800

    Update metadatav1-osp.sh

--- metadatav1-osp.sh
+++ metadatav1-osp.sh
@@ -135,12 +135,14 @@ filterFunction(){
 
 
     else echo "These are the server that will be patched: $filterServerType";
-        filterSpecificPath="$filterFilePath-$filterServerType.csv";
+        #filterSpecificPath="$filterFilePath-$filterServerType.csv";
+        filterSpecificPath="./sample_os_patching_filter.csv";
         awk -v batch="$filterBatch" -v region="$filterRegion" -v type=$filterServerType -F',' '$2==batch && $4==region && $5==type' $filterSourcePath > $filterSpecificPath
 
         login_enabled=`aws iam get-login-profile --user-name "MichaelDennisCresido"`
         echo $login_enabled
-        commandOutput=$(createSSMCommandFunction "$filterEnvironment" $filterSourcePath)
+        commandOutput=$(createSSMCommandFunction "$filterEnvironment" "$filterSpecificPath")
+        #commandOutput=$(createSSMCommandFunction "$filterEnvironment" $filterSourcePath)
         echo $commandOutput
 
         sleep 2
