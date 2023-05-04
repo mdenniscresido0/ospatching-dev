de30d00b2c7deb37fa60970ba1033c3a12248d66
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-04 19:49:52 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -4,7 +4,7 @@ print('Hello, world!')
 ssm_client = boto3.client('ssm',region_name='us-east-1')
 response = ssm_client.send_command(
             InstanceIds=['i-01571d416d841669e'],
-            DocumentName="AWS-RunShellScript",
+            DocumentName="AWS-RunPowerShellScript",
             Parameters={'commands': ['date']}, )
 
 command_id = response['Command']['CommandId']
