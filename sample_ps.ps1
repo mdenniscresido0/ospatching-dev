3bd509106cb1fabc938aaa46a3bc9d83c271cf5b
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-04 19:58:54 +0800

    Update sample_ps.ps1

--- sample_ps.ps1
+++ sample_ps.ps1
@@ -1,7 +1,6 @@
-﻿$servername=$env:ComputerName
 $date=get-date
 
-Write-Output "Today is $date. This is executed in server: $servername"
+echo "Today is $date."
 
 
-Send-SSMCommand -Region us-east-1 -DocumentName "AWS-RunPowerShellScript" -Parameter @{commands = "date"} -InstanceId i-186ce5b8
+Send-SSMCommand -Region us-east-1 -DocumentName "AWS-RunPowerShellScript" -Parameter @{commands = "date"} -InstanceId i-01571d416d841669e
