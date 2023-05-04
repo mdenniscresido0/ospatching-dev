1d610d17cfb702f5bbfbb9e893ea78b3c4b246bd
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-04 20:09:17 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -1,11 +1,20 @@
 # This program prints Hello, world!
 import boto3
+import pandas
+
 print('Hello, world!')
 ssm_client = boto3.client('ssm',region_name='us-east-1')
 response = ssm_client.send_command(
-            InstanceIds=['i-01571d416d841669e'],
-            DocumentName="AWS-RunPowerShellScript",
+            InstanceIds=['i-186ce5b8'],
+            DocumentName="AWS-RunShellScript",
             Parameters={'commands': ['date']}, )
 
 command_id = response['Command']['CommandId']
 print(command_id)
+
+pandas.set_option('expand_frame_repr', False)
+df = pandas.read_csv('c:\\yay\\sample1.csv')
+print('---Here are all 7 lines---')
+print(df)
+print('---Here are the first 5 lines---')
+print(df.head())
