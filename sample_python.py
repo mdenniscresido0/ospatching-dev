812f5446b083d7f7dadb6f94a62f3ca620221f95
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-04 21:43:27 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -15,14 +15,21 @@ print(command_id)
 
 
 
-f = open('sample_os_patching.csv')
-csv_f = csv.reader(f)
+#f = open('sample_os_patching.csv')
+#csv_f = csv.reader(f)
 
 product = 'pim'
 server = 'db'
 #filtered = filter(lambda p: ('pim' == p[1] and 'db' == p[4]) , csv_f)
-result = filter(lambda p: (product == p[1] and server == p[4]) , csv_f)
+#result = filter(lambda p: (product == p[1] and server == p[4]) , csv_f)
 
-print(list(result))
-for e in result:
-    print(e)
+#print(list(result))
+#for e in result:
+   # print(e)
+
+with open('test_csv.csv', 'r') as input_file:
+    csv_reader = csv.reader(input_file)#, delimiter=',')
+    lines = [i for i in csv_reader]
+    header = lines[0]
+    results = filter(lambda row: (product == row[1] and server == row[4]), lines[1:])
+    print(results)
