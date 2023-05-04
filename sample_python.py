a8fc4bdb8697c19c2971914c0781f43b4e93e70b
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-04 19:42:31 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -1,9 +1,7 @@
 # This program prints Hello, world!
 import boto3
-ssm_client = boto3.client('ssm')
-
 print('Hello, world!')
-
+ssm_client = boto3.client('ssm',region_name='us-east-1')
 response = ssm_client.send_command(
             InstanceIds=['i-186ce5b8'],
             DocumentName="AWS-RunShellScript",
