e31e7f3d8945c8c938f0e11a3c4fbb7b7cf49193
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-07 22:28:56 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -11,15 +11,17 @@ try:
 
     product = 'pim'
     server = 'db'
+    exception='pim5'
         #filtered = filter(lambda p: ('pim' == p[1] and 'db' == p[4]) , csv_f)
-    result = filter(lambda p: (product == p[1] and server == p[4]), csv_f)
+    resultI = filter(lambda p: (exception != p[0]), csv_f)
+    result = filter(lambda p: (product == p[1] and server == p[4]), resultI)
 
 
     for e in result:
 
         print(type(e))
         print(e[0], e[2])
-        #print(e[0] - e[1] - e[2] - e[3] - e[4] - e[5] - e[6])
+
 except IndexError:
     print('except block ran')
     
