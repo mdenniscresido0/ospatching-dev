1c7fb21aacadf853d8ef3adeeb3e77fa47ed67e0
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-07 22:34:28 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -11,9 +11,12 @@ try:
 
     product = 'pim'
     server = 'db'
-    exception='pim5'
-        #filtered = filter(lambda p: ('pim' == p[1] and 'db' == p[4]) , csv_f)
-    resultI = filter(lambda p: (exception != p[0]), csv_f)
+    exception='pim5, pim6, pim7'
+    exception_list = exception.split(",")
+       
+    for x in exception_list:
+        resultI = filter(lambda p: (x != p[0]), csv_f)
+    
     result = filter(lambda p: (product == p[1] and server == p[4]), resultI)
 
 
