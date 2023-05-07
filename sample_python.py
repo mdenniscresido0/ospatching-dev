5ea8c90affb3c2cce2c0416d3488b1b554eb64c5
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-07 19:47:50 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -24,4 +24,4 @@ with open('sample_os_patching.csv', 'rt') as f:
         if row:
             if product == row[0] and server == row[4]:
                 cfd_checked_before = "Yes"
-                print(row[0],row[1],row[2],row[3],row[4],row[5])
+                print(row[0] - row[1] - row[2] - row[3] - row[4] - row[5])
