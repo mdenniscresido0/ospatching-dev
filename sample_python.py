65715babff26280897707988ccc42731ead096bb
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-07 19:54:05 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -15,13 +15,28 @@ print(command_id)
 
 
 
+f = open('sample_os_patching.csv')
+csv_f = csv.reader(f)
 
 product = 'pim'
 server = 'db'
-with open('sample_os_patching.csv', 'rt') as f:
-    reader = csv.reader(f, delimiter=',')
-    for row in reader:
-        if row:
-            if product == row[0] and server == row[4]:
-                cfd_checked_before = "Yes"
-                print(row[0] - row[1] - row[2] - row[3] - row[4] - row[5])
+#filtered = filter(lambda p: ('pim' == p[1] and 'db' == p[4]) , csv_f)
+result = filter(lambda p: (product == p[1] and server == p[4]), csv_f)
+
+
+for e in result:
+
+    try: 
+        ssm_client = boto3.client('ssm',region_name='us-east-1')
+        response = ssm_client.send_command(
+                    InstanceIds=['i-01571d416d841669e'],
+                    Comment='test-command',
+                    DocumentName="AWS-RunPowerShellScript",
+                    Parameters={'commands': ['date']}, )
+
+        command_id = response['Command']['CommandId']
+        print(command_id)
+        print(e)
+    except IndexError:
+        print('except block ran')
+        continue
