564c4ab33153aedc94377dfd0c087c4b2e0f9dd6
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-09 18:38:07 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -27,22 +27,18 @@ try:
     #result = resultF
     #result = filter(lambda p: (exception_list[1] != p[0]), result)
     #result = resultF
-    resultFinal = []
-    for exception_item in exception_list:
-        print(exception_item)
-        resultFinal = filter(lambda p: (exception_item != p[0]), result)
-        result = resultFinal
-        for e in result:
+    count = len(exception_list)
+    i = 0
+    result = filter(lambda p: (exception_list[0] != p[0]), result)
+    result = filter(lambda p: (exception_list[1] != p[0]), result)
+    result = filter(lambda p: (exception_list[2] != p[0]), result)
+    #while i < count:
+        #result = filter(lambda p: (exception_list[i] != p[0]), result)
+        #i = i + 1
+    for e in result:
 
-            #print(type(e))
-            print(e[0], e[1], e[2], e[3], e[4], e[5], e[6])
-
-
-
-        
-    
-    #result = filter(lambda p: (product == p[1] and server == p[4]), resultI)
-
+        #print(type(e))
+        print(e[0], e[1], e[2], e[3], e[4], e[5], e[6])        
 
 
 except IndexError:
