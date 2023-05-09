aebeefb27f2e69983e53025c00a1d34c9a247e89
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-09 23:41:40 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -1,37 +1,127 @@
-# This program prints Hello, world!
-import boto3
-import csv
-import sys
-import pandas as pd
-import os
+def mainFunction():
+    import boto3
+    import csv
+    import sys
+    import pandas as pd
+    import os
 
-print('Hello, world!')
-env = os.environ['AWS Environment']
-reg = os.environ['Region']
-server = os.environ['Server Type']
-product = os.environ['Product Name']
-
-print(product, server, env, reg)
-
-try:
     
-    exception = "pim5,pim6,pim1"
+    inputException = os.environ['Product Exception']
+    inputRegion = os.environ['Region']
+    inputServerType = os.environ['Server Type']
+    inputProduct = os.environ['Product Name']
 
-    df = pd.read_csv('sample_os_patching.csv')
 
-    filter_val = df.query('batch == "pim" & server_lookup == "db"')
-    
-    if exception == "":
-        filter_val = filter_val
+    mainRegion=caseRegion(inputRegion)
+    mainServerType=caseServerType(inputServerType)
+    mainProduct=caseProductName(inputProduct)
+    mainException=inputException
+
+    mainCommand=filterData(mainRegion, mainServerType, mainProduct, mainException)
+    print(mainCommand)
+    print('Hello, world!')
+
+
+def filterData(runRegion, runServerType, runProduct, runException):
+
+    filter_df = pd.read_csv('sample_os_patching.csv')
+    filterProduct = filter_df.query('batch == @runProduct')
+
+    if runServerType == "all" and runRegion == "all":
+        filterCSVTable = filterProduct
+    elif runServerType == "all" and runRegion != "all":
+        filterCSVTable = filterProduct.query('reg_lookup == @runRegion')
+    elif runServerType != "all" and runRegion != "all":
+        filterCSVTable = filterProduct.query('server_lookup == @runServerType')
     else:
-        exception_list = exception.split(",")
-        for e in exception_list:
-            filtered = filter_val.query('product != @e')
-            filter_val = filtered        
-    
-    for index, row in filter_val.iterrows():
-        print(row['product'], row['batch'], row['reg'], row['server_lookup'], row['key'], row['value'])
-    
+        filterCSVTable = filterProduct.query('reg_lookup == @runRegion & server_lookup == @runServerType')
 
-except IndexError:
-    print('except block ran')
+    if runException == "":
+        filterCSVTable = filterCSVTable
+    else:
+        runExceptionList = runException.split(",")
+        for runProductFilter in runExceptionList:
+            filterDataFinal = filterCSVTable.query('product != @runProductFilter')
+            filterCSVTable = filterDataFinal        
+    
+    for index, row in filterCSVTable.iterrows():
+
+        print(row['product'], row['batch'], row['reg'], row['server_lookup'], row['key'], row['value'])
+
+
+#def runSSMCommand():
+    #print
+
+def caseProductName(productName):
+    match productName:
+        case "Ajera":
+            return "ajera"
+
+        case "Citrix":
+            return "citrix"
+
+        case "DFME_Maconomy-ESSENTIALS":
+            return "dfme"
+
+        case "DFVE_Vision-Vantagepoint-ESSENTIALS":
+            return "dfve"
+
+        case "MacEnt-Pod-5_and_14":
+            return "mn5-14"
+
+        case "Pod-MN11":
+            return "mn11"
+
+        case "Pod-MN3":
+            return "mn3"
+
+        case "Maconomy-ENTERPRISE":
+            return "macent"
+
+        case "Vision-Vantagepoint-ENTERPRISE":
+            return "vtent"
+
+        case "PIM":
+            return "pim"
+
+        case "Deltek-Dev":
+            return "dev"
+
+def caseServerType(serverType):
+    match serverType:
+        case "DB":
+            return "db"
+
+        case "NonDB":
+            return "nondb"
+
+        case "ALL":
+            return "all"     
+        
+def caseRegion(region):
+    match region:
+        case "US":
+            return "us"
+
+        case "EU":
+            return "eu"
+
+        case "CA":
+            return "ca"     
+
+        case "AP":
+            return "ap" 
+
+        case "ALL":
+            return "all"    
+
+def caseDocument(documentType):
+    match documentType:
+        case "db":
+            return "AWS-RunPowerShellScript"
+        case "nondb":
+            return "AWS-RunPowerShellScript"
+
+
+mainFunction()
+ 
