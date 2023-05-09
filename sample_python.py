7a5f61676793098749da47e075cf7e8362c8a376
Dennis Cresido <100140522+mdenniscresido0@users.noreply.github.com>
2023-05-09 18:22:16 +0800

    Update sample_python.py

--- sample_python.py
+++ sample_python.py
@@ -35,7 +35,7 @@ try:
     #result = filter(lambda p: (product == p[1] and server == p[4]), resultI)
 
 
-    for e in resultFinal:
+    for e in result:
 
         #print(type(e))
         print(e[0], e[1], e[2], e[3], e[4], e[5], e[6])
